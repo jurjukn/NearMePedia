cbed15fc51be54b116d2c530cc492d9422910db5
Jurgis Juknevicius <jurjukn@gmail.com>
2019-12-14 23:44:53 +0100

    implemented delete for list items

--- near-me-pedia/src/components/SavedArticles/savedArticlesList/DisplaySavedArticles.js
+++ near-me-pedia/src/components/SavedArticles/savedArticlesList/DisplaySavedArticles.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FlatList } from 'react-native';
 import { useSelector } from 'react-redux'
 import { Item } from './Item'
@@ -6,10 +6,14 @@ import { Item } from './Item'
 export const DisplaySavedArticles = () => {
 
     const savedArticles = useSelector(state => state.savedArticles)
+    useEffect(()=>{
+        console.log("saved articles changes")
+     }, savedArticles)
+
     return(
         <FlatList
             data={savedArticles}
-            renderItem={({ item }) => <Item articleLat={item.lat} articleLong={item.lon} title={item.title} key={item.title} />}
+            renderItem={({ item }) => <Item article={item} articleLat={item.lat} articleLong={item.lon} title={item.title} key={item.title} />}
             keyExtractor={item => item.title}
         />
     )
--- near-me-pedia/src/components/SavedLocations/locationsList/Item.js
+++ near-me-pedia/src/components/SavedLocations/locationsList/Item.js
@@ -1,19 +1,37 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
-export const Item = ({ latitude, longitude, coordinates, address, navigate }) => {
+import { useSelector, useDispatch } from 'react-redux'
 
+import { assignLocations } from './../../../state_manager/actions'
+
+export const Item = ({ latitude, longitude, coordinates, address, navigate, location }) => {
+
+    const savedLocations = useSelector(state => state.savedLocations)
+    const dispatch = useDispatch();
 
     const assignCoordsAndNavigate = (coordinates, navigate) =>{
         navigate.navigate('Nearby',{coordinates: coordinates})
     }
     
+    const deleteLocation = (location) => {
+        const newLocationsList = savedLocations.filter(
+            (item) => (
+                item.coordinates.latitude !== location.coordinates.latitude
+                && item.coordinates.longitude !== location.coordinates.longitude
+            )
+            
+        )
+        dispatch(assignLocations(newLocationsList))
+    }
+
     return (
         <View style={styles.item}>
             <View onStartShouldSetResponder={()=>assignCoordsAndNavigate(coordinates, navigate)}>
                 <Text style={styles.title}> {address} </Text>
             </View>
             <Text>{latitude} : {longitude}</Text>
+            <Button title="delete" onPress={()=>deleteLocation(location)} />
         </View>
     )
 }
